f9f60a187128b582e1be4013a4ddb8659f87549d
Alex D <pr0fedt@gmail.com>
2023-08-23 11:56:16 +0200

    add textfield

---
+++ components/input/styled.ts
@@ -0,0 +1,117 @@
+import styled from "styled-components";
+
+export const classNames = {
+  Focused: "focused",
+  Hovered: "hovered",
+  Filled: "filled",
+  Error: "error",
+  Pending: "pending",
+} as const;
+
+export const InputBox = styled.div`
+  width: 100%;
+
+  span {
+    position: relative;
+    height: 20px;
+    background: transparent;
+  }
+
+  .input-container {
+    position: relative;
+    display: flex;
+  }
+
+  .input {
+    border: none;
+    outline: none;
+    width: 100%;
+    height: 20px;
+    background: transparent;
+    caret-color: var(--text-primary);
+    color: transparent;
+    margin: 4px;
+    &::-ms-clear {
+      display: none;
+    }
+
+    &__area {
+      transition: 300ms ease-out;
+      padding: 10px 14px;
+      background: var(--fill-secondary);
+      border-radius: 16px;
+      min-height: 60px;
+    }
+    &__hint {
+      color: var(--text-secondary);
+      font-size: 12px;
+      line-height: 16px;
+      letter-spacing: 0.5px;
+      transition: 300ms ease-out;
+      margin: 2px 4px 2px 6px;
+    }
+  }
+  .suggestions {
+    content: " ";
+    height: 34px;
+    gap: 6px;
+    margin-top: 7px;
+    display: flex;
+    opacity: 0;
+    pointer-events: none;
+
+    &__item {
+      box-shadow: 0px 2px 1px rgba(51, 20, 0, 0.04),
+        0px 4px 8px rgba(51, 20, 0, 0.08);
+      border-radius: 16px;
+      letter-spacing: 0.03em;
+      font-variation-settings: "wght" 700;
+      padding: 4px 8px;
+      height: 32px;
+      background: var(--fill-secondary);
+      color: var(--text-primary);
+    }
+  }
+
+  &.${classNames.Filled} {
+    .input {
+      color: var(--text-primary);
+    }
+  }
+
+  &.${classNames.Focused} {
+    .input {
+      &__area {
+        background: transparent;
+        box-shadow: inset 0 0 0 2px var(--border-focus);
+      }
+    }
+    .suggestions {
+      opacity: 1;
+      pointer-events: all;
+    }
+  }
+
+  &.${classNames.Error} {
+    .input {
+      &__area {
+        background: var(--fill-negative-bg);
+        box-shadow: inset 0 0 0 1px var(--border-negative);
+      }
+      &__hint {
+        color: var(--border-negative);
+      }
+    }
+  }
+
+  &.${classNames.Pending} {
+    pointer-events: none;
+    background: transparent;
+    caret-color: #000;
+    border: 0;
+    outline: 0;
+    .input {
+      pointer-events: none;
+    }
+  }
+`;
---
+++ components/input/text-field.tsx
@@ -0,0 +1,59 @@
+import { ChangeEvent, forwardRef, MouseEvent, ReactNode } from "react";
+import { classNames, InputBox } from "./styled";
+import Noop from "../helpers/noop";
+
+interface Props {
+  className?: string;
+  children?: ReactNode;
+  value?: string;
+  placeholder?: string;
+  tabIndex?: number;
+  title?: string;
+  reverseHint?: boolean;
+  onChange?: (e: ChangeEvent<HTMLInputElement>) => void;
+  onClick?: (e: MouseEvent<HTMLDivElement>) => void;
+}
+
+const TextField = forwardRef<HTMLInputElement, Props>((props, ref) => {
+  const container = (
+    <div className="input-container">
+      <input
+        key="container"
+        ref={ref}
+        tabIndex={props.tabIndex}
+        className="input"
+        placeholder={props.placeholder}
+        value={props.value}
+        onChange={props.onChange}
+      />
+
+      {props.children}
+    </div>
+  );
+
+  const hint = (
+    <div key="hint" className="input__hint">
+      {props.title}
+    </div>
+  );
+
+  return (
+    <InputBox className={props.className} onClick={props.onClick}>
+      <div className="input__area">
+        {props.reverseHint ? (
+          <>
+            {container}
+            {hint}
+          </>
+        ) : (
+          <>
+            {hint}
+            {container}
+          </>
+        )}
+      </div>
+    </InputBox>
+  );
+});
+
+export default Object.assign({}, TextField, classNames);
